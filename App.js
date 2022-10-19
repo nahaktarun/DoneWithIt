a0b44c843e80a13bd4cce68912ade805b88f1de5
tarun nahak <tarunnahak25@gmail.com>
2022-10-19 11:29:24 +0530

    Added button

--- App.js
+++ App.js
@@ -1,24 +1,53 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-  Image,
-  Button,
-  Alert,
-} from "react-native";
-import { useDimensions } from "@react-native-community/hooks";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "./app/components/AppButton";
 
-export default function App() {
-  return <ViewImageScreen />;
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      blurRadius={10}
+      style={styles.background}
+      source={require("./app/assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("./app/assets/logo-red.png")}
+        />
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" />
+        <AppButton title="Register" color="secondary" />
+      </View>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
   },
 });
+
+export default WelcomeScreen;
