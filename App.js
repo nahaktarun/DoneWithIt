abe7a19f49b0b47467299e929afab8f157b435d6
tarun nahak <tarunnahak25@gmail.com>
2022-10-21 23:16:26 +0530

    added the icon component

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
@@ -36,7 +38,11 @@ function App(props) {
 
     // <ListingDetailsScreen />
     // <ViewImageScreen />
-    <MessagesScreen />
+    // <MessagesScreen />
+
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+    </Screen>
   );
 }
 
